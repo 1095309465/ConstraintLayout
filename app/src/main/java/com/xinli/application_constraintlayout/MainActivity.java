5f9c3a152a6387bb479387e041e7db10963e4be9
1095309465 <1095309465@qq.com>
2020-02-12 01:14:25 +0800

    FIRST COMMINT

--- app/src/main/java/com/xinli/application_constraintlayout/MainActivity.java
+++ app/src/main/java/com/xinli/application_constraintlayout/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//    https://www.jianshu.com/p/17ec9bd6ca8a,contraintlayout使用讲解
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
